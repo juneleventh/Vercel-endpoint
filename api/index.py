775a3d792e5487cca7a6cb076169c97249825aaa
juneleventh <25ds3000002@ds.study.iitm.ac.in>
2026-06-07 18:26:27 +0530

    Create index.py

---
+++ api/index.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI, Query
+from fastapi.middleware.cors import CORSMiddleware
+import json
+
+app = FastAPI(title="JSON read")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
+
+@app.post('/latency')
+def region_metrics():
+    metrics = {
+  {
+    "regions": "apac",
+    "avg_latency": "171.3825",
+    "p95_latency": "209.5625",
+    "avg_uptime ": "98.42858333",
+    "breaches": "8"
+  },
+  {
+    "regions": "emea",
+    "avg_latency": "173.6325",
+    "p95_latency": "210.3925",
+    "avg_uptime ": "98.52191667",
+    "breaches": "8"
+  }
+    }
+    return metrics
+
